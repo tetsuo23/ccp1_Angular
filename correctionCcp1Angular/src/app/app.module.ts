d9b191a34c40a73efe36767c1049eab4f77f72d3
tetsuo23 <carretmo@gmail.com>
2020-03-16 22:25:47 +0100

    add HTML + CSS

--- correctionCcp1Angular/src/app/app.module.ts
+++ correctionCcp1Angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
